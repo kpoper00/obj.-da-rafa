21683ef9dfce76fe5a9947121f55fa4a9d5c2566
kpoper00 <114153584+kpoper00@users.noreply.github.com>
2024-05-07 12:32:01 -0300

    ok

--- main.js
+++ main.js
@@ -42,14 +42,14 @@ function calculaTempo(tempoObjetivo) {
 }
 
 function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
     document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
     document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
     document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
     document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
-
-    for (let i=0; i<contadores.length;i++){
-        contadores[i].textContent = calculaTempo(tempos[i]);   
-    }
+    console.log("seg"+i);
+  
+   }
 }
 
 function comecaCronometro(){
