74849c34163e6490416800184b1c0b377fe6ca40
kpoper00 <114153584+kpoper00@users.noreply.github.com>
2024-05-07 12:04:31 -0300

    ok

--- main.js
+++ main.js
@@ -16,9 +16,9 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-11-25T24:00:00");
-const tempoObjetivo2 = new Date("2024-12-255T13:00:00");
+const tempoObjetivo2 = new Date("2024-12-25T13:00:00");
 const tempoObjetivo3 = new Date("2024-12-25T05:00:00");
-const tempoObjetivo4 = new Date("2025-11-01T03:00:00");
+const tempoObjetivo4 = new Date("2025-11-25T03:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
